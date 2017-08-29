6f538651550ecabf4742e00ddd3b0a9d68e188cc
Jake Champion <me@jakechampion.name>
2017-08-29 13:26:11 +0100

    Add intersectionRatio and isIntersecting to the prototype

--- polyfill/intersection-observer.js
+++ polyfill/intersection-observer.js
@@ -59,23 +59,38 @@ function IntersectionObserverEntry(entry) {
   this.rootBounds = entry.rootBounds;
   this.boundingClientRect = entry.boundingClientRect;
   this.intersectionRect = entry.intersectionRect || getEmptyRect();
-  this.isIntersecting = !!entry.intersectionRect;
-
-  // Calculates the intersection ratio.
-  var targetRect = this.boundingClientRect;
-  var targetArea = targetRect.width * targetRect.height;
-  var intersectionRect = this.intersectionRect;
-  var intersectionArea = intersectionRect.width * intersectionRect.height;
-
-  // Sets intersection ratio.
-  if (targetArea) {
-    this.intersectionRatio = intersectionArea / targetArea;
-  } else {
-    // If area is zero and is intersecting, sets to 1, otherwise to 0
-    this.intersectionRatio = this.isIntersecting ? 1 : 0;
-  }
+  
+  // Private properties.
+  this._entry = entry;
 }
 
+Object.defineProperty(IntersectionObserverEntry.prototype,
+  'intersectionRatio', {
+    get: function () {
+      // Calculates the intersection ratio.
+      var targetRect = this.boundingClientRect;
+      var targetArea = targetRect.width * targetRect.height;
+      var intersectionRect = this.intersectionRect;
+      var intersectionArea = intersectionRect.width * intersectionRect.height;
+
+      // Sets intersection ratio.
+      if (targetArea) {
+        return intersectionArea / targetArea;
+      } else {
+        // If area is zero and is intersecting, sets to 1, otherwise to 0
+        return this.isIntersecting ? 1 : 0;
+      }
+    }
+  }
+);
+
+Object.defineProperty(IntersectionObserverEntry.prototype,
+  'isIntersecting', {
+    get: function () {
+      return !!this._entry.intersectionRect;
+    }
+  }
+);
 
 /**
  * Creates the global IntersectionObserver constructor.
