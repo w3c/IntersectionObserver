e384f5295ff243fddc74423fd94663b4134a8f07
siusin <siusinng@gmail.com>
2017-09-13 22:30:03 +0800

    add polyfill into the file list

--- compile.sh
+++ compile.sh
@@ -6,4 +6,7 @@ bikeshed spec
 if [ -d out ]; then
   echo Copy the generated spec into out/index.html
   cp index.html out/index.html
+
+  echo Copying the polyfill into out/
+  cp polyfill/intersection-observer-test.html polyfill/intersection-observer-test.js polyfill/intersection-observer.js out/polyfill/
 fi
