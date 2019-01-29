51c0744931c4d4a6045434356930117bb6d2a6d2
Stefan Zager <szager@chromium.org>
2019-01-29 15:21:18 -0800

    Update deploy script to add branch-specific subdirectories

--- compile.sh
+++ compile.sh
@@ -3,7 +3,12 @@ set -e # Exit with nonzero exit code if anything fails
 
 bikeshed spec
 
-if [ -d out ]; then
-  echo Copy the generated spec into out/index.html
-  cp index.html out/index.html
+outdir=out
+if [ -n "$1" ]; then
+  outdir="$1"
+fi
+
+if [ -d "${outdir}" ]; then
+  echo Copy the generated spec into "${outdir}/index.html"
+  cp index.html "${outdir}/index.html"
 fi
