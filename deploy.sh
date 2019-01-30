0af67f7d03d127c71e57dacbd6c6252888599823
Stefan Zager <szager@chromium.org>
2019-01-29 16:18:52 -0800

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -43,30 +43,32 @@ mkdir -p "${outdir}"
 doCompile "${outdir}"
 
 # Now let's go have some fun with the cloned repo
-cd "${gh_pages_dir}"
-git config user.name "Travis CI"
-git config user.email "$COMMIT_AUTHOR_EMAIL"
+( cd "${gh_pages_dir}" &&
+  git config --local user.name "Travis CI" &&
+  git config --local user.email "$COMMIT_AUTHOR_EMAIL" )
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if [ -z "$(git status -s -u)" ]; then
+if [ -z "$(cd ${gh_pages_dir} && git status -s -u)" ]; then
     echo "No changes to the output on this push; exiting."
     exit 0
 fi
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
-git add -A .
-git commit -m "Deploy to GitHub Pages: ${SHA}"
+( cd "${gh_pages_dir}" &&
+  git add -A . &&
+  git commit -m "Deploy to GitHub Pages: ${SHA}" )
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $encrypted_781c04ca6946_key -iv $encrypted_781c04ca6946_iv -in ../deploy_key.enc -out ../deploy_key -d
-chmod 600 ../deploy_key
+openssl aes-256-cbc -K $encrypted_781c04ca6946_key -iv $encrypted_781c04ca6946_iv -in deploy_key.enc -out deploy_key -d
+chmod 600 deploy_key
 eval `ssh-agent -s`
-ssh-add ../deploy_key
+ssh-add deploy_key
 
 # Now that we're all set up, we can push.
-git push $SSH_REPO $TARGET_BRANCH
+( cd "${gh_pages_dir}" &&
+  git push $SSH_REPO $TARGET_BRANCH )
